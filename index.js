a9ebac0013e54d36bbe8dc41bc32e8f9ffa6703f
valopezs <valeriabrilopez@gmail.com>
2022-08-30 17:18:09 -0500

    added package.json and dependencies

--- index.js
+++ index.js
@@ -185,3 +185,28 @@ const addEmployee = () => {
     })
 };
 
+// function to generate HTML page file using file system
+const writeFile  = data => {
+    fs.writeFile('./dist/index.html', data, err => {
+        // if there is an error 
+        if (err) {
+            console.log(err);
+            return;
+        // when the profile has been created 
+        } else {
+            console.log("Your team profile has been successfully created! Please check out the index.html")
+        }
+    })
+};
+
+addManager()
+  .then(addEmployee)
+  .then(teamArray => {
+    return generateHTML(teamArray);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .catch(err => {
+ console.log(err);
+  });
